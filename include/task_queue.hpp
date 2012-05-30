14a8d496021b1a1463436f2d745619cbcc575c1a
nick <goodfella005@gmail.com>
2012-05-29 20:57:41 -0700

    Updated some comments in the task_queue class:
    * Added exception safety information.

--- include/task_queue.hpp
+++ include/task_queue.hpp
@@ -10,6 +10,24 @@ namespace libtq
 {
     class itask;
 
+    /** Queues and waits on individual tasks
+     *
+     *  This task queue allows threads to wait on arbitrary tasks
+     *  without affecting the task runner thread.  Note that there is
+     *  a little extra overhead to support waiting on arbitrary tasks.
+     *
+     *  @exception std::exception Unless specifically noted in a
+     *  method's documentation, task_queue methods make no attempt to
+     *  catch any exception thrown from the underlying STL container.
+     *  So a std exception generated from an operation on said
+     *  container will propagate to the caller.
+     *
+     *  @par Exception Safety:
+     *  Unless specifically noted in a method's documentation, all methods
+     *  have a strong exception guarantee in that if an exception is thrown,
+     *  the task queue is in exactly the same state it was before the method
+     *  was called.
+     */
     class task_queue
     {
 	public:
@@ -19,30 +37,37 @@ namespace libtq
 
 	/** Starts the task queue's task runner thread
 	 *
-	 *  @return zero if the task runner thread was successfully
+	 *  @return Zero if the task runner thread was successfully
 	 *  started, non-zero otherwise.
 	 */
 	int start_queue();
 
 	/** Shutdowns the task queue's task runner thread
 	 *
-	 *  The calling thread will block until the task runner has
-	 *  been shut down successfully.  This method waits until all
-	 *  the tasks queued at the time it's called are finished.
+	 *  This method blocks the calling thread until all the tasks
+	 *  queued at the time it's called are finished, and the task
+	 *  runner thread is stopped.
 	 */
 	void shutdown_queue();
 
 	/** Stops the task runner thread
 	 *
 	 *  This method stops the task runner thread immediately.
-	 *  After this method is finished there may still be tasks
-	 *  queued.
+	 *
+	 *  @note After this method is finished there may still be
+	 *  tasks queued.
+	 *
+	 *  @par Exception Safety:
+         *  This method has a no-throw guarantee.
 	 */
 	void stop_queue();
 
 	/** Cancels any tasks, and stops the runner thread
 	 *
 	 *  This method calls cancel_tasks and stop_queue atomically.
+	 *
+	 *  @par Exception Safety:
+	 *  This method has a no-throw guarantee.
 	 */
 	void cancel_queue();
 
@@ -57,6 +82,7 @@ namespace libtq
 	 *  A task can only be queued once.  So if the task was
 	 *  already queued and is not yet complete, this function will
 	 *  not queue the task a second time.
+	 *
 	 */
 	void queue_task(itask * const task);
 
@@ -66,20 +92,26 @@ namespace libtq
 	 *  completes or the task is canceled.  If the task is not
 	 *  already queued, this function exits immediately.
 	 *
-	 *  @return greater than zero if the task was executed while
+	 *  @return Greater than zero if the task was executed while
 	 *  being waited on, less than zero if the task was canceled
 	 *  while being waited on, or zero if the task was not already
 	 *  scheduled.
+	 *
+	 *  @par Exception Safety:
+	 *  This method has a no-throw guarantee.
 	 */
 	int wait_for_task(itask * const task);
 
 	/** Cancels the execution of a task
 	 *
 	 *  Cancels a previously queued task, and notifies all the
-	 *  threads waiting on the task that the task is complete.
+	 *  threads waiting on the task that the task was canceled.
 	 *
-	 *  @return true if the task was canceled, false if the task
+	 *  @return True if the task was canceled, false if the task
 	 *  was not scheduled at the time this method was called.
+	 *
+	 *  @par Exception Safety:
+	 *  This method has a no-throw guarantee.
 	 */
 	bool cancel_task(itask * const task);
 
